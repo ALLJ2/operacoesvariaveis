4d44f5baedc98574a293eb94d8c364b2832c3a2d
alexsandrolucianodejesus <alexsandro.ljesus@senacsp.edu.br>
2024-04-29 15:38:38 -0300

    operações

---
+++ Const-num-main/js/script.js
@@ -0,0 +1,96 @@
+//Exibindo o numero inteiro
+console.log(2);
+
+
+//Exibindo o tipo de dado na tela
+console.log(typeof 2.5);
+
+
+// Imprimindo utilizando uma constante
+
+const meunumero = 10;
+console.log(meunumero);
+
+
+//  Erro ao alterar o const
+// const numero = 10;
+// numero = 3;
+// console.log(numero);
+
+
+// Operação de adição
+console.log(1+3);
+
+
+// Operação de subtração
+console.log(4-2);
+
+
+// Operação de multiplicação
+console.log(2*2);
+
+
+// Operação de divisão
+console.log(10/2);
+
+
+// Operação de módulo ou resto da divisão
+console.log(3%2);
+
+
+// Variavel que pode ser alterada
+let testeTroca = 10;
+
+testeTroca = 5;
+console.log(testeTroca);
+
+
+// Exibindo soma através das variaveis
+const a = 1;
+const b = 2;
+console.log (a + b);
+
+
+// Operação com precedência
+console.log((1 + 1)*4**2);
+console.log((2-2)*(4+4));
+
+
+// Not A Number e Infinity / numeros especiais
+console.log(0/0);
+console.log(10/3);
+console.log(typeof Infinity);
+
+
+// Letras / strings
+// Strings utilizando aspas simples e aspas duplas
+console.log('Olá mundo!');
+console.log("Olá planeta terra");
+
+
+// contagem de strings
+const nome = "Ana Claudia";
+console.log(nome.length);
+
+// String de numeros
+const valores = "123";
+console.log(valores);
+console.log(typeof valores);
+
+
+// Pular uma linha na string
+console.log("pular uma linha para mostrar o resultado \nR$125,00");
+
+// Espaçamento na string
+console.log("dar um espaço \tno texto");
+
+// concatenação
+const meuTexto = "Olá" + "mundo!";
+console.log(meuTexto);
+
+const meuTexto2 = "Olá" .concat("mundo novamente");
+console.log(meuTexto2);
+
+const minhaIdade = 18;
+console.log("a minha idade é " + minhaIdade);
+
